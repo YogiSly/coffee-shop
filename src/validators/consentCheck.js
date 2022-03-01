a294f47abe0581b1bb4562f27c105f1dcd43ecee
YogiSly <84290221+YogiSly@users.noreply.github.com>
2022-03-01 22:07:38 +0300

    add vuelidate

---
+++ src/validators/consentCheck.js
@@ -0,0 +1,3 @@
+export const consentCheck = (value) => {
+  return value;
+};
--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -23,7 +23,7 @@
 
             <form @submit.prevent="submit" action="#" class="mt-5">
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="name-input" class="mb-0">
                     Name
                     <span style="color: red">*</span>
@@ -34,13 +34,16 @@
                     type="text"
                     class="form-control"
                     id="name-input"
-                    v-model="form.name"
+                    v-model="v$.name.$model"
                   />
+                  <span v-for="error in v$.name.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="email-input" class="mb-0">
                     E-mail
                     <span style="color: red">*</span>
@@ -51,13 +54,16 @@
                     type="email"
                     class="form-control"
                     id="email-input"
-                    v-model="form.email"
+                    v-model="v$.email.$model"
                   />
+                  <span v-for="error in v$.email.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
               <div class="form-group row">
-                <div class="col col-12 col-sm-3 d-flex align-items-center">
+                <div class="col col-12 col-sm-3 d-flex align-items-start">
                   <label for="phone-input" class="mb-0"> Phone </label>
                 </div>
                 <div class="col col-12 col-sm-9">
@@ -65,13 +71,13 @@
                     type="tel"
                     class="form-control"
                     id="phone-input"
-                    v-model="form.phone"
+                    v-model="phone"
                   />
                 </div>
               </div>
 
               <div class="form-group row textarea">
-                <div class="col col-12 d-flex justify-content-center">
+                <div class="col col-12 d-flex justify-content-start">
                   <label for="pmessage" class="mb-3 mt-3 text-center">
                     Your message
                     <span style="color: red">*</span>
@@ -79,16 +85,38 @@
                 </div>
                 <div class="col col-12">
                   <textarea
-                    v-model="form.message"
+                    v-model="v$.message.$model"
                     class="form-control"
                     name="message"
                     id="message"
                     rows="5"
                     placeholder="Leave your comments here"
                   ></textarea>
+                  <span v-for="error in v$.message.$errors" :key="error.$uid">{{
+                    error.$message
+                  }}</span>
                 </div>
               </div>
 
+              <div class="form-group row">
+                <div class="col col-12 col-sm-9 d-flex align-items-start">
+                  <label for="consent-input" class="mb-0"
+                    >Consent to the processing of personal data</label
+                  >
+                </div>
+                <div class="col col-12 col-sm-3">
+                  <input
+                    type="checkbox"
+                    class="form-check-input"
+                    id="consent-input"
+                    v-model="v$.consent.$model"
+                    checked
+                  />
+                </div>
+              </div>
+              <span v-for="error in v$.consent.$errors" :key="error.$uid">{{
+                error.$message
+              }}</span>
               <div class="row">
                 <div class="col">
                   <button class="btn btn-outline-dark send-btn">Send us</button>
@@ -103,23 +131,56 @@
 </template>
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
+import useVuelidate from "@vuelidate/core";
+import { required, email, maxLength } from "@vuelidate/validators";
+import { helpers } from "@vuelidate/validators";
+import { minLength } from "../validators/minLength";
+import { consentCheck } from "../validators/consentCheck";
+
 export default {
-  components: {
-    NavBarComponent,
+  setup() {
+    return { v$: useVuelidate() };
   },
   data() {
     return {
-      form: {
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      consent: "true",
+    };
+  },
+  components: {
+    NavBarComponent,
+  },
+  validations() {
+    return {
+      name: { required },
+      email: { required, email },
+      phone: {},
+      message: {
+        required,
+        maxLength: maxLength(20),
+        minLength: helpers.withMessage(" this value min 5", minLength),
+      },
+      consent: {
+        required,
+        consentCheck: helpers.withMessage(" this not checked", consentCheck),
       },
     };
   },
+
   methods: {
-    submit() {
-      console.log(this.form);
+    async submit() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) return;
+      console.log({
+        name: this.name,
+        phone: this.phone,
+        email: this.email,
+        message: this.message,
+        consent: this.consent,
+      });
     },
   },
 };
